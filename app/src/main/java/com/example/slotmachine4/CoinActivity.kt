c210c688fbcb492c71ab11eeaf74775d9c500e92
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2021-06-01 13:38:16 +0300

    Add coin activity_coin for gift for user and add logic with animation

--- app/src/main/java/com/example/slotmachine4/CoinActivity.kt
+++ app/src/main/java/com/example/slotmachine4/CoinActivity.kt
@@ -1,12 +1,20 @@
 package com.example.slotmachine4
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color.BLACK
 import android.os.Bundle
+import android.view.Gravity.CENTER_HORIZONTAL
+import android.view.View
+import android.widget.TextView
+import android.widget.ViewSwitcher
+import androidx.appcompat.app.AppCompatActivity
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import com.example.slotmachine4.R.drawable.*
 import com.example.slotmachine4.databinding.ActivityCoinBinding
 import com.example.slotmachine4.game.Game
 import com.example.slotmachine4.game.PrefsKeys
-import com.example.slotmachine4.view.startActivity
+
 
 class CoinActivity : AppCompatActivity() {
     lateinit var binding: ActivityCoinBinding
@@ -17,11 +25,37 @@ class CoinActivity : AppCompatActivity() {
         binding = ActivityCoinBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.apply {
+            usersGiftTextView.setFactory(ViewSwitcher.ViewFactory {
+                val textView = TextView(this@CoinActivity)
+                textView.setTextColor(BLACK)
+                textView.textSize = 60F
+                textView.gravity = CENTER_HORIZONTAL
+                textView
+            })
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
         val pref = getSharedPreferences(PrefsKeys.SETTING, Context.MODE_PRIVATE)
         val gameProcess = Game(pref)
 
-        gameProcess.makeGiftForUser()
+        binding.apply {
+            YoYo.with(Techniques.Tada)
+                    .duration(500)
+                    .repeat(5)
+                    .playOn(giftButton)
 
-        startActivity<MainActivity>()
+            giftButton.setOnClickListener{
+                giftButton.setImageResource(ic_open_gift)
+
+                usersGiftTextView.visibility = View.VISIBLE
+                usersGiftTextView.setText("+50")
+
+                gameProcess.makeGiftForUser()
+            }
+        }
     }
 }
